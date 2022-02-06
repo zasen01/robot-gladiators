c5f3af6ed83323b76f87aa01ac2298041d120cb3
zasen01 <zse0296@gmail.com>
2022-02-06 16:26:23 -0500

    Created index and js files

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+//Prompts User to input robot name
+var playerName = window.prompt("What is you robot's name?");
+console.log(playerName);
+//Mesage to Yourself
+console.log("This logs a string, Good for leaving yourself a message");
+//Math
+console.log(10 +10);
+//Players Robots Name
+console.log("Our robots name is " + playerName);
+
+// This Creates a function name 'fight'
+//function fight()
+/*{
+    window.alert("The Fight has begun!");
+}*/
